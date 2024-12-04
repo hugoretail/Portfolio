732108016ec849299ae6f22e5b2bc07c6196eece
Retail Hugo <hugo.retail@u-bordeaux.fr>
2024-12-04 11:27:25 +0100

    supplementary fix and multiple functions for readability

--- src/computer-science/scripts/retex.js
+++ src/computer-science/scripts/retex.js
@@ -1,10 +1,20 @@
 const updateImagePaths = (selectedLanguage) => {
     const images = document.querySelectorAll('img[data-lang-path]');
     images.forEach(img => {
-        const basePath = `./assets/img/${selectedLanguage}/`;
+        const basePath = `../../assets/img/${selectedLanguage}/`;
         const fileName = img.getAttribute('data-lang-path');
         img.src = `${basePath}${fileName}`;
     });
+};
+
+const applyTheme = (theme) => {
+    if (theme === 'dark') {
+        document.body.classList.add('dark-mode');
+        document.body.classList.remove('light-mode');
+    } else {
+        document.body.classList.add('light-mode');
+        document.body.classList.remove('dark-mode');
+    }
 };
 
 const selectedLanguage = sessionStorage.getItem("selectedLanguage") || navigator.language.split('-')[0];
@@ -12,14 +22,7 @@ const supportedLanguages = ['en', 'fr', 'ja'];
 const defaultLanguage = supportedLanguages.includes(selectedLanguage) ? selectedLanguage : 'en';
 
 document.documentElement.lang = defaultLanguage;
-sessionStorage.setItem("selectedLanguage", defaultLanguage);
 updateImagePaths(defaultLanguage);
 
-document.getElementById("language-select").value = defaultLanguage;
-
-document.getElementById("language-select").addEventListener("change", () => {
-    const newLanguage = document.getElementById("language-select").value;
-    document.documentElement.lang = newLanguage;
-    sessionStorage.setItem("selectedLanguage", newLanguage);
-    updateImagePaths(newLanguage);
-});
+const currentTheme = sessionStorage.getItem('theme') || 'light';
+applyTheme(currentTheme);
